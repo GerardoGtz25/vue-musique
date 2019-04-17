a1df6b0922ae3e64755721f9928a9e5555066c5d
Gerardo Gutierrez <ggutierrez@grainchian.io>
2019-04-17 12:54:50 -0500

    Implementation de vuex

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="container">
+  <div class="container" v-if="track && track.album">
     <div class="columns">
-      <div class="column is-3 has-text-centered" v-if="track && track.album">
+      <div class="column is-3 has-text-centered">
         <figure class="media-left">
           <p class="image">
             <img :src="track.album.images[0].url">
           </p>
-          <p>
+          <p class="button-bar">
             <a class="button is-primary is-large">
-              <span class="icon" @click="selectTrack"></span>
+              <span class="icon icon is-small" @click="selectTrack">
+                <i class="fas fa-play"></i>
+              </span>
             </a>
           </p>
         </figure>
@@ -16,7 +18,7 @@
       <div class="column is-8">
         <div class="panel">
           <div class="panel-heading">
-            <h1 class="title">{{ track.name }}</h1>
+            <h1 class="title">{{ trackTitle }}</h1>
           </div>
           <div class="panel-block">
             <article class="media">
@@ -32,7 +34,7 @@
               </div>
               <nav class="level">
                 <div class="level-left">
-                  <a class="level-item"></a>
+                  <a class="level-item">  </a>
                 </div>
               </nav>
             </article>
@@ -44,23 +46,34 @@
 </template>
 
 <script>
-import trackService from './../service/track'
+// import trackService from './../service/track'
 import trackMixin from '@/mixins/track'
+
+import { mapState, mapActions, mapGetters } from 'vuex'
 
 export default {
   mixins: [ trackMixin ],
 
-  data() {
-    return {
-      track: {}
-    }
+  computed: {
+    ...mapState(['track']),
+    ...mapGetters(['trackTitle'])
   },
 
   created() {
     const id = this.$route.params.id
-    trackService.getById(id).then(res => {
-      this.track = res
-    })
+
+    if (!this.track || !this.track.id || this.track.id !== id) {
+      this.getTrackById({ id }).then(() => {
+        console.log('Track loaded...')
+      })
+    }
+    // trackService.getById(id).then(res => {
+    //   this.track = res
+    // })
+  },
+
+  methods: {
+    ...mapActions(['getTrackById'])
   }
 }
 </script>
@@ -69,6 +82,8 @@ export default {
   .columns {
     margin: 20px;
   }
+  .button-bar {
+    margin-top: 20px;
+  }
 </style>
-
 
--- src/store.js
+++ src/store.js
@@ -1,16 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import trackService from './service/track'
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    track: {}
   },
+
+  getters: {
+    trackTitle(state) {
+      if(!state.track.name) { return '' }
+      `${state.track.name} - ${state.track.artists[0].name}`
+      return 
+    }
+  },
+
   mutations: {
-
+    setTrack(state, track) {
+      state.track = track
+    }
   },
-  actions: {
 
+  actions: {
+    getTrackById(context, playload) {
+      return trackService.getById(playload.id).then(res => {
+        context.commit('setTrack', res)
+        return res
+      })
+    }
   },
 });
